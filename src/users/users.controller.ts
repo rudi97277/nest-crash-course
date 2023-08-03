004d393d2235c5986703a28bde7aaa38cd384720
rudi97277 <rudi97277@gmail.com>
2023-08-03 22:13:07 +0700

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  getUsers(): any {
+    return [{ id: 0 }];
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): any {
+    return { id };
+  }
+}
