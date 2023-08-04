0f925f2fe70c5bb4cb281dd7b6897ed25e116e10
Rudianto Sihombing <rudi97277@gmail.com>
2023-08-04 13:19:18 +0700

    feat: finishing crash course

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,52 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
+  @ApiOkResponse({ type: User, isArray: true })
+  @ApiQuery({ name: 'name', required: false })
   @Get()
-  getUsers(): any {
-    return [{ id: 0 }];
+  getUsers(@Query('name') name: string): User[] {
+    return this.usersService.findAll(name);
   }
 
+  @ApiOkResponse({ type: User })
+  @ApiNotFoundResponse()
   @Get(':id')
-  getUserById(@Param('id') id: string): any {
-    return { id };
+  getUserById(@Param('id', ParseIntPipe) id: number): User {
+    const user = this.usersService.findById(id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  @ApiCreatedResponse({ type: User })
+  @ApiBadRequestResponse()
+  @Post()
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
   }
 }
